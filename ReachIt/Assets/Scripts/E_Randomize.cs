a40915263d80e5a1367af4a6178e10db0476f024
Elif Gümüştaş <elif_gumus_tas@hotmail.com>
2023-11-16 13:38:24 +0300

    VisualStudio Code intelisens
    Trying to implement visual studio code intelisense

---
+++ ReachIt/Assets/Scripts/E_Randomize.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Randomize : MonoBehaviour
+{
+    private int _height;
+    private int _width;
+    private int _speed;
+    private Vector3 _nextBuilding;
+
+    void Start()
+    {
+        
+    }
+
+    void RandomThings()
+    {
+        Debug.Log("random mami");
+
+        
+    }
+}
+    
