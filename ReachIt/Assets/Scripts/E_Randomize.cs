c6bec8f94cc37184ba7b55efb7b1d7f27418600d
Elif Gümüştaş <elif_gumus_tas@hotmail.com>
2023-12-13 16:19:11 +0300

    Trying out
    I am trying to find a way to change the pivot of the obcject so that  i can work better (If i use only top point of the rectangle then i would use coordinates better).
    I did try to only instantine if in cameraframe, it worked but because i want it there to be an effect of rising in clouds from top, i need to figure out the spawn points again.

--- ReachIt/Assets/Scripts/E_Randomize.cs
+++ ReachIt/Assets/Scripts/E_Randomize.cs
@@ -6,13 +6,14 @@ using DG.Tweening;
 public class E_Randomize : MonoBehaviour
 {
     public GameObject buildingPrefab;
+    public Camera mainCamera;
     [System.NonSerialized] public float height;
     [System.NonSerialized] public float endPointy;
     [System.NonSerialized] public float width;
     [System.NonSerialized] public float speed;
     [System.NonSerialized] public Vector3 nextBuildingPosition;
     private float _nextBuildingX;
-    private float _nextBuildingY = -14f;
+    private float _nextBuildingY = -5f;
     [System.NonSerialized] private  float _oldbuildingstartPosX = -9;
 
     void RandomThings()
@@ -21,7 +22,7 @@ public class E_Randomize : MonoBehaviour
         height = Random.Range(5,11);
 
         //Random Ending point.Y
-        endPointy = Random.Range(-6,0);
+        endPointy = Random.Range(-1,3);
 
         //Random building width
         width = Random.Range(5,10);
@@ -36,30 +37,59 @@ public class E_Randomize : MonoBehaviour
         
     }
 
+    //After testing it change it back to IEnumerator and tryout spawn times
     void SpawnBuilding()
     {
         RandomThings();
         
         nextBuildingPosition = new Vector3( _oldbuildingstartPosX + width + _nextBuildingX , _nextBuildingY, 0);
 
-        var buildingInstance = Instantiate(buildingPrefab, nextBuildingPosition, Quaternion.identity); 
+        // Convert the random position to world coordinates
+        Vector3 worldPosition = mainCamera.ViewportToWorldPoint(nextBuildingPosition);
 
-        buildingInstance.transform.DOMoveY(endPointy, speed);
+        //Debug.Log("worldposition" + worldPosition);
 
-        //Changing the building's width [Vector3.right => (1,0,0)] height  [Vector3.up => (0,1,0)]
-        buildingInstance.transform.localScale = Vector3.right * width + Vector3.up * (height);
+        //if(IsInCameraView(worldPosition))
+       // {
+            var buildingInstance = Instantiate(buildingPrefab, nextBuildingPosition, Quaternion.identity); 
+
+            SetPivotToTop(buildingInstance.transform);
+
+            buildingInstance.transform.DOMoveY(endPointy, speed);
+
+            //Changing the building's width [Vector3.right => (1,0,0)] height  [Vector3.up => (0,1,0)]
+            buildingInstance.transform.localScale = Vector3.right * width + Vector3.up * (6f);
+       // }
 
         _oldbuildingstartPosX = nextBuildingPosition.x;
 
+       // yield return new WaitForSeconds(.1f);
+
     }
+
+    bool IsInCameraView(Vector3 bPosition)
+    {
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(bPosition);
+
+        return viewportPosition.x >= -1 && viewportPosition.x <= 1 && viewportPosition.y >= -1 && viewportPosition.y <= 1;
+    }
+
+    void SetPivotToTop(Transform transform)
+    {
+        Vector3 topPosition = new Vector3(transform.position.x, transform.position.y + transform.localScale.y / 2, transform.position.z);
+
+        transform.position = topPosition;
+     }
 
 
     void Update() 
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+
+        if(Input.GetKeyDown("space"))
         {
             SpawnBuilding();
         }
+       //StartCoroutine(SpawnBuilding());
     }
 }
     
